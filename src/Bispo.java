645802c82b03725bafefe954841113a0dded785b
miguelbertoldi <116910190+miguelbertoldi@users.noreply.github.com>
2023-05-31 21:56:54 -0300

    .

--- src/Bispo.java
+++ src/Bispo.java
@@ -4,6 +4,9 @@ public class Bispo extends Peca{
 
     @Override
     public ArrayList<Posicao> possiveisMovimentos(Tabuleiro tabuleiro) {
+        for (Posicao posicao : tabuleiro.getPosicoes()) {
+
+        }
         return null;
     }
 }
