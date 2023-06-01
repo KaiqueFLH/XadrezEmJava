0bde5a2465493ad080f44b9635d2313847ee4a05
miguelbertoldi <116910190+miguelbertoldi@users.noreply.github.com>
2023-05-31 21:37:42 -0300

    .

---
+++ src/Jogador.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Jogador {
+    private ArrayList<Peca> pecas = new ArrayList<>();
+    private String nome;
+    private String cor;
+    private double pontos;
+    private String senha;
+
+    public boolean moverPeca(Peca peca, Posicao posicao) {
+        return true;
+    }
+
+    public boolean proporEmpate(Jogador jogador) {
+        return true;
+    }
+
+    public void desistir() {
+
+    }
+
+
+}
---
+++ src/Tabuleiro.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Tabuleiro {
+    private ArrayList<Posicao> posicoes;
+
+    public void removerPeca(Posicao posicao) {
+
+    }
+
+    public ArrayList<Posicao> getPosicoes() {
+        return posicoes;
+    }
+
+    public void setPosicoes(ArrayList<Posicao> posicoes) {
+        this.posicoes = posicoes;
+    }
+}
